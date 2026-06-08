efc926601c095b81b881cbdc6d7d1f4b9e2941ae
zhanghaorui <zhanghaorui.sx@gmail.com>
2026-06-08 22:17:27 +0800

    chore: init project - Spring Boot 3.3 / Java 17 with Web, Lombok, JUnit

---
+++ src/main/java/com/practice/workflow/WorkflowTaskLabApplication.java
@@ -0,0 +1,13 @@
+package com.practice.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowTaskLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowTaskLabApplication.class, args);
+    }
+}
+
---
+++ src/test/java/com/practice/workflow/WorkflowTaskLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowTaskLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
